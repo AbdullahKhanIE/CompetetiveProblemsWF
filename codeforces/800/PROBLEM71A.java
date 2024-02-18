7df2d00f883d57620cb3217767987897caaca63d
Shahriar Asif <unknownboy398@gmail.com>
2024-02-19 00:47:32 +0600

    Add files via upload

---
+++ codeforces/800/PROBLEM71A.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PROBLEM71A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] word= new String[n];
+        for (int i = 0; i < n; i++) {
+            word[i] = sc.nextLine();
+        }
+        for (int i = 0; i < n; i++) {
+            int index = word[i].length();
+            String temp = word[i];
+            if (index > 10)
+                System.out.println(temp.charAt(0) + Integer.toString(index - 2) + temp.charAt(index - 1));
+            else
+                System.out.println(temp);
+        }
+
+    }
+}
