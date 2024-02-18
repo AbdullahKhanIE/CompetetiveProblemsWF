ab9fcec2369bf7f7ef8b617468ae7be6f1d8ff90
ENO <abdullahkhan.ie.official@gmail.com>
2024-02-19 01:45:16 +0600

    Update 74A.cpp

--- codeforces/800/74A.cpp
+++ codeforces/800/74A.cpp
@@ -2,17 +2,19 @@
 using namespace std;
 
 int main(){
-    int n;
-    cin>>n;
-    while (n--) {
-        string word;
-        cin>>word;
-        int z = word.length();
-        if (z>10) {
-            cout<<word[0]<<z-2<<word[z-1]<<endl;
-        }
-        else {
-            cout<<word<<endl;
+    int input;
+    string word;
+    cin>>input;
+    if (input > 0 && input < 101){
+        while (input--){
+            cin>>word;
+            int x = word.length();
+            if (x > 10){
+                cout<<word[0]<<x - 2<<word[x - 1]<<endl;
+            }
+            else{
+                cout<<word<<endl;
+            }
         }
     }
     return 0;
