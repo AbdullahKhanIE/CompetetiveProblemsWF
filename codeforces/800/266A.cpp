257632f525768de46f98aa5ca82edc2edd74f4c5
ENO <abdullahkhan.ie.official@gmail.com>
2024-02-20 03:12:37 +0600

    266 617 A

---
+++ codeforces/800/266A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    int n,count=0;
+    cin>>n;
+    string word;
+    cin>>word;
+    
+    for (int i = 0; i < n-1; i++) {
+        if (word[i] == word[i+1]) {
+            ++count;
+        }
+    }
+    cout<<count<<endl;
+    return 0;
+}
