0d97499849cbf9df1206161bd93d60a8efbe78c3
whyAsif <unknownboy398@gmail.com>
2024-02-19 14:56:06 +0600

    solved 112A

--- codeforces/800/PROBLEM112A.java
+++ codeforces/800/PROBLEM112A.java
@@ -3,23 +3,21 @@ import java.util.Scanner;
 public class PROBLEM112A {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int count1 = 0, count2 = 0;
         String word1 = sc.nextLine();
         String word2 = sc.nextLine();
-        word1 = word1.toLowerCase();
-        word2 = word2.toLowerCase();
 
-        for (int i = 0; i < word1.length(); i++) 
-            count1 += (int) word1.charAt(i);
-        for (int i = 0; i < word2.length(); i++) 
-            count2 += (int) word2.charAt(i);
-        if (count1 == count2)
-            System.out.println("0");
-        else if (count1 < count2)
-            System.out.println("-1");
-        else if (count2 < count1)
-            System.out.println("1");
-            
+        for (int i = 0; i < word1.length(); i++){
+            if(word1.toLowerCase().charAt(i)==word2.toLowerCase().charAt(i))
+                continue;
+            else if(word1.charAt(i)>word2.charAt(i)){
+                System.out.println(1);
+                break;
+            }
+            else if(word1.charAt(i)<word2.charAt(i)){
+                System.out.println(-1);
+                break;
+            }
+        }
         sc.close();
     }
 }
