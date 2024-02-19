44009ef0fac4e9eda515828b671013481127a710
whyAsif <unknownboy398@gmail.com>
2024-02-19 15:33:03 +0600

    solved, but not optimized imo

--- codeforces/800/PROBLEM112A.java
+++ codeforces/800/PROBLEM112A.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
-
 public class PROBLEM112A {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String word1 = sc.nextLine();
-        String word2 = sc.nextLine();
-
-        for (int i = 0; i < word1.length(); i++){
-            if(word1.toLowerCase().charAt(i)==word2.toLowerCase().charAt(i))
+        String word1 = sc.nextLine().toLowerCase();
+        String word2 = sc.nextLine().toLowerCase();
+        if (word1.equals(word2))
+            System.out.println(0);
+        for (int i = 0; i < word1.length(); i++) {
+            if (word1.charAt(i) == word2.charAt(i))
                 continue;
-            else if(word1.charAt(i)>word2.charAt(i)){
+            else if (word1.charAt(i) > word2.charAt(i)) {
                 System.out.println(1);
                 break;
-            }
-            else if(word1.charAt(i)<word2.charAt(i)){
+            } else if (word1.charAt(i) < word2.charAt(i)) {
                 System.out.println(-1);
                 break;
             }
